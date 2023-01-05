38c12295abd6b58e9a1cd40aef7fedd74db0614c
felicitasamanta <samantafelicita@gmail.com>
2023-01-06 00:44:12 +0200

    changes

--- src/components/form.js
+++ src/components/form.js
@@ -1,16 +1,19 @@
 import { useState } from "react";
+import "./style.css";
 
 function Form() {
-  const [talpa, setTalpa] = useState('');
-  const [greitis, setGreitis] = useState('');
-  const [trukme, setTrukme] = useState('');
-  const [result, setResult] = useState('');
+  const [talpa, setTalpa] = useState("");
+  const [greitis, setGreitis] = useState("");
+  const [trukme, setTrukme] = useState("");
+  const [result, setResult] = useState("");
+  const [isActive, setIsActive] = useState(false);
 
   const skaiciuotiHandler = () => {
+    setIsActive((current) => !current);
     setResult(((trukme * greitis) / talpa) * 100);
-    setTalpa('');
-    setGreitis('');
-    setTrukme('');
+    setTalpa("");
+    setGreitis("");
+    setTrukme("");
   };
 
   const changeTalpaHandler = (event) => {
@@ -23,10 +26,21 @@ function Form() {
   const changeTrukmeHandler = (event) => {
     setTrukme(event.target.value);
   };
+  function getAlertStyle(value) {
+    if (value >= 100) {
+      return "alert-danger";
+    } else if (value >= 90) {
+      return "alert-secondary";
+    } else if (value >= 80) {
+      return "alert-success";
+    } else {
+      return "";
+    }
+  }
 
   return (
     <div className="card bg-primary bg-opacity-25">
-      <div className="card-header bg-primary bg-opacity-50 mb-5 fs-1 text-center">
+      <div className="card-header bg-primary bg-opacity-50 mb-3 fs-1 text-center">
         Lietaus rezervuaro užpildymas
       </div>
       <div className="card-body">
@@ -41,6 +55,7 @@ function Form() {
               id="talpa"
               value={talpa}
               onChange={changeTalpaHandler}
+              placeholder="0"
             />
           </div>
           <div class="mb-3">
@@ -53,10 +68,11 @@ function Form() {
               id="greitis"
               value={greitis}
               onChange={changeGreitisHandler}
+              placeholder="0"
             />
           </div>
           <div class="mb-3">
-            <label for="trukme" class="form-label ">
+            <label for="trukme" class="form-label fs-6">
               Laikas, kiek rezervuaras bus pildomas (val.)
             </label>
             <input
@@ -65,6 +81,7 @@ function Form() {
               id="trukme"
               value={trukme}
               onChange={changeTrukmeHandler}
+              placeholder="0"
             />
           </div>
 
@@ -77,9 +94,23 @@ function Form() {
           </button>
         </form>
       </div>
-
-      <div>{result} procentų</div>
+      {/* //kaip paslepti div teksta, kol dar nera atspausdinta? */}
+      <div class={`alert alert-primary d-flex fs-6`}>
+        <p className={isActive ? "" : "hide"}>
+          {" "}
+          Rezervuaras bus pripildytas {result}%
+        </p>
+      </div>
+      
     </div>
   );
 }
 export default Form;
+{
+  /* <div class={`alert ${getAlertStyle(result)} alert-primary d-flex fs-6`}>
+<p className={isActive ? "" : "hide"}>
+  {" "}
+  Rezervuaras bus pripildytas {result}%
+</p>
+</div> */
+}
